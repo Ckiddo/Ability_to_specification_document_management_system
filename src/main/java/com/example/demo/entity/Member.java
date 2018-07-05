6b85a8331066beab11f54e3254f26b4a857b15a8
陈康 <chenkang@chenkangdeMacBook.local>
2018-07-05 23:47:09 +0800

    不管Member了

--- src/main/java/com/example/demo/entity/Member.java
+++ src/main/java/com/example/demo/entity/Member.java
@@ -1,12 +1,18 @@
 package com.example.demo.entity;
 
+import lombok.Data;
+
+
+
+@Data
 public class Member {
-    String name;
-    String sex;
-    java.util.Date birthdate;
-    String homeAddress;
-    String contactInfor;
-    String referees;
-    String IndustryBranch;
-    String specialCommittee;
+    private String name;
+    private String password;
+    private String sex;
+    private String birthdate;
+    private String homeAddress;
+    private String contactInfor;
+    private String referees;
+    private String IndustryBranch;
+    private String specialCommittee;
 }
